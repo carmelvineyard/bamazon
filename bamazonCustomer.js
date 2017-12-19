8a0c7b29ea45af90e5e8950288a0e762db653c72
Carmel Johnson <vineyardcarmel@gmail.com>
2017-12-18 22:47:51 -0500

    saved status as of 12/18 @ 10:47pm. Code is further along but not functional.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,8 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var choice = 0;
+var quantity = 0;
+var order = [choice, quantity];
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -15,18 +18,72 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("Mischief Managed!");
+  console.log(":::::::::::::::::::::::::\nWelcome to Bamazon!\n:::::::::::::::::::::::::");
   readProducts();
 });
 
 function readProducts() {
-  console.log("Selecting all products...\n");
-  connection.query("SELECT * FROM products", function(err, res) {
+  console.log("Loading product list...\n:::::::::::::::::::::::::");
+  connection.query("SELECT item_id, product_name, price FROM products", function(err, res) {
     if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+          console.log("Item ID: " + res[i].item_id + " || Name: " + res[i].product_name + " || Price: " + res[i].price + "\n");
+    }
     // Log all results of the SELECT statement
-    var legible = JSON.stringify(res, null, 2);
-    console.log(legible);
-    connection.end();
+   // var legible = JSON.stringify(res, null, 2);
+   // console.log(legible);
+    takeOrder();
   });
 }
+
+function takeOrder() {
+	inquirer
+	.prompt([
+		{
+			name: "userChoice",		    
+		    type: "input",
+		    message: "Which item number would you like to order?",
+		    validate: function(value) {
+		      	if (isNaN(value) === false) {
+		      		return true;
+		      		choice = (this.value);
+		      	}
+		      	return false;
+		    }
+	    },
+	    {
+	    	name: "userQuantity",
+	    	type: "input",
+	    	message:"How many would you like?",
+	    	validate: function(value) {
+	    		if (isNaN(value) === false) {
+	    			return true;
+	    			quantity = (this.value);
+	    		}
+	    		return false;
+	    	}
+	    }
+	])  // .prompt end
+	.then(function(order))
+	console.log("Order = [choice, quantity]: " + order);
+	connection.end();
+}
+
 //connection.end();
+/*
+pseudocode:::::::
+
+functions needed:
+	user starts program.
+	<done>program displays all items available for sale:
+		(item_id, name, price)
+	<done>Inquirer asks the ID of the product they want to buy
+	<done>Inquirer asks what quantity of the item they want
+
+	Program checks to see if store has enough of the item
+		if not, program should console.log "Insufficient Quantity!"
+			and stop order.
+		if so, program should "fulfill the order":
+			update database to reflect remaining quantity
+			show user total cost of purchase
+*/
